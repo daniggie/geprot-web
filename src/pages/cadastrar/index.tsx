e554d6da1ea2c12f8c6daa42c9992c52f49ff15d
Brendon <brendon3007gabriel@gmail.com>
2021-07-14 15:20:25 -0300

    Cadastrar comecado

--- src/pages/cadastrar/index.tsx
+++ src/pages/cadastrar/index.tsx
@@ -1,14 +1,27 @@
 import React, { useState, FormEvent } from 'react';
-import { Barra } from './style';
+import { Barra, Titulo, Formulario, Caixa } from './style';
 import arrowleft from '../../icons/arrowleft.svg';
 
 const Cadastrar: React.FC = () => {
     return (
+      <>
         <Barra>
             <img src={arrowleft}/>
-
-            
         </Barra>
+        <Titulo>
+          <h1>CADASTRO DE PRODUTOS</h1>
+        </Titulo>
+        <Formulario>
+          <h1>IDENTIFICADORES</h1>
+          <Caixa>
+            <div>
+
+            </div>
+
+          </Caixa>
+        </Formulario>
+
+        </>
     );
 }
 export default Cadastrar;
--- src/pages/cadastrar/style.ts
+++ src/pages/cadastrar/style.ts
@@ -1,11 +1,48 @@
 import styled from 'styled-components';
 
 export const Barra = styled.div`
-    height: 50px;
-    width: 100%;
-    box-shadow:  0px 1px #dedede;
-    img {
-        margin-left: 15px;
-        margin-top:15px;
-    }
+  height: 50px;
+  width: 100%;
+  box-shadow:  0px 1.5px #dedede;
+  img {
+      margin-left: 15px;
+      margin-top: 9px;
+      width:20px;
+      height:30px;
+  };
+`;
+
+export const Titulo = styled.div`
+  height: 80px;
+  width: 100%;
+  margin:0px;
+
+  h1{
+    color:#00579d;
+    font-size: 20px;
+    line-height: 23.44px;
+    padding-top: 40px;
+    padding-left: 75px;
+  };
+`;
+
+export const Formulario = styled.form`
+height: 500px;
+width: 90%;
+margin: 0px auto;
+border:  1px solid #dedede;
+h1{
+  color:#0091BD;
+  font-size: 18px;
+  line-height: 21.09px;
+  padding-top: 40px;
+  padding-left: 75px;
+}
+`;
+
+export const Caixa = styled.div`
+height: 350px;
+width: 90%;
+margin: 15px auto;
+border:  1px solid #dedede;
 `;
