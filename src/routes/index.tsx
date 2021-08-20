4e7302ad44e4b5ab29951aee06a3ba472f0747cc
CarlosHolz <carlos.holz07@gmail.com>
2021-08-20 08:27:42 -0300

    apns

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Cadastrar from '../pages/cadastrar';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Dashboard from '../pages/Dashboards';
+import Route from './Route';
 
 
 const Routes: React.FC = () => (
   <Switch>
-  <Route path="/" exact component={Login} />
-  <Route path="/home" exact component={Home} />
-  <Route path="/dashboard" exact component={Dashboard} />
-  <Route path="/cadastrar" exact component={Cadastrar} />
+    <Route path="/" exact component={Login} />
+    <Route path="/home" component={Home} />
+    <Route path="/dashboard" component={Dashboard} isPrivate />
+    <Route path="/cadastrar" component={Cadastrar} />
   </Switch>
 );
 
