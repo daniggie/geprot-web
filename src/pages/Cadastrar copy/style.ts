efee5487f321da49201ffb9229c6e406618bb817
daniggie <dani.gross53@gmail.com>
2021-08-27 07:55:11 -0300

    rename cadastro

---
+++ src/pages/Cadastrar copy/style.ts
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+
+export const Titulo = styled.div`
+  margin-top: 30px;
+  margin-left: 30px;
+`;
+
+export const Formulario = styled.form`
+margin: 20px;
+padding: 60px;
+border:  1px solid #dedede;
+
+display:flex;
+justify-content: space-between;
+align-items: flex-start;
+flex-direction: row;
+flex-wrap: wrap;
+
+p{
+  width: 100%;
+}
+
+.content{
+  width: 45%;
+  height: 290px;
+  display:flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: space-evenly;
+
+
+  .line{
+    width: 100%;
+    display flex;
+
+    flex-direction: row;
+    flex-wrap: nowrap;
+    align-items: center;
+    justify-content: space-between;
+    b{
+      width: 45%;
+    }
+
+    .tamanho{
+      width:45%;
+    }
+
+    input{
+      width: 100%;
+      box-shadow: inset 2px 4px 3px rgba(0,0,0,0.25);
+      border-radius:5px;
+      border: 1px;
+      padding:10px;
+    }
+  }
+}
+
+.position{
+  width:100%;
+    display:flex;
+    justify-content: flex-end;
+  }
+`;
