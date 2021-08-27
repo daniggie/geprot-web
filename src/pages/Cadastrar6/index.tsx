259b675fa3810f87005db00c9e3c4802f594a055
daniggie <dani.gross53@gmail.com>
2021-08-27 11:37:05 -0300

    Cadastrar 5/6

--- src/pages/Cadastrar6/index.tsx
+++ src/pages/Cadastrar6/index.tsx
@@ -1,10 +1,212 @@
-import React, { useState, FormEvent } from 'react';
-import { } from './style';
+import React  from 'react';
+import { Titulo, Formulario } from './style';
+import Barra from "../../components/Barra";
+import BotaoAvancar from '../../components/ButtonAvancar';
+import { BsFillCaretRightFill, BsX } from "react-icons/bs";
+import { FiX } from "react-icons/fi";
+import { RiAddLine } from "react-icons/ri";
 
 const Cadastrar6: React.FC = () => {
     return (
       <>
-        <h1>CADASTRAR 6</h1>
+        <a href="/cadastrar5">
+        <Barra>
+        </Barra>
+      </a>
+
+      <Titulo>
+          <p className="helvetica fonte_20 cor_5 bold">Resumo</p>
+        </Titulo>
+        <Titulo>
+          <b className="helvetica fonte_15 cor_0">Cadastro de projetos</b>
+          <BsFillCaretRightFill color="#828282"/>
+          <b className="helvetica fonte_15 cor_0">Aprovações</b>
+          <BsFillCaretRightFill color="#828282"/>
+          <b className="helvetica fonte_15 cor_0">Verbas</b>
+          <BsFillCaretRightFill color="#828282"/>
+          <b className="helvetica fonte_15 cor_0">Centro de custos</b>
+          <BsFillCaretRightFill color="#828282"/>
+          <b className="helvetica fonte_15 cor_0">Alocação </b>
+          <BsFillCaretRightFill color="#828282"/>
+          <b className="helvetica fonte_15 cor_5 normal"> <u> Resumo </u> </b>
+        </Titulo>
+        <Formulario>
+          <p className="helvetica fonte_25 cor_3 bold"> NOME DO PROJETO</p>
+          <p className="helvetica fonte_20 cor_3 lighter">Seção X <b className="helvetica fonte_20 cor_0 lighter">123456</b></p>
+          <div className="content">
+
+            <div className="line">
+              <b className="helvetica fonte_20 cor_3 bold">Geral</b>
+            </div>
+
+            <div className="line">
+              <div className="tamanho">
+                <b className="helvetica fonte_13 cor_3 bold">ID PPM:</b>
+              </div>
+              <div className="tamanho">
+                <b className="helvetica fonte_13 cor_0 lighter">00000001</b>
+              </div>
+            </div>
+
+            <div className="line">
+              <div className="tamanho">
+                <b className="helvetica fonte_13 cor_3 bold">RESPONSÁVEL:</b>
+              </div>
+              <div className="tamanho">
+                <b className="helvetica fonte_13 cor_0 lighter">Lara Hanna</b>
+              </div>
+            </div>
+
+            <div className="line">
+              <div className="tamanho">
+                <b className="helvetica fonte_13 cor_3 bold">SOLICITANTE:</b>
+              </div>
+              <div className="tamanho">
+                <b className="helvetica fonte_13 cor_0 lighter">Rodrigo Loudres</b>
+              </div>
+            </div>
+
+            <div className="line">
+              <div className="tamanho">
+                <b className="helvetica fonte_13 cor_3 bold">PREVISÃO</b>
+              </div>
+              <div className="tamanho">
+                <b className="helvetica fonte_13 cor_0 lighter">13/05/2021 á 25/02/2022</b>
+              </div>
+            </div>
+
+            <div className="line">
+              <div className="tamanho">
+                <b className="helvetica fonte_13 cor_3 bold">VERBA:</b>
+              </div>
+              <div className="tamanho">
+                <b className="helvetica fonte_13 cor_0 lighter">R$    80.000,00</b>
+              </div>
+            </div>
+
+            <div className="line">
+              <b className="helvetica fonte_20 cor_3 bold">Centro de custos</b>
+            </div>
+
+            <div className="line">
+              <div className="tamanho2">
+                <b className="helvetica fonte_13 cor_3 bold">NOME: </b>
+                <b className="helvetica fonte_13 cor_0 lighter"> Setor</b>
+              </div>
+              <div className="tamanho2">
+                <b className="helvetica fonte_13 cor_3 bold">RESPONSÁVEL: </b>
+                <b className="helvetica fonte_13 cor_0 lighter"> Nome responsável</b>
+              </div>
+              <div className="tamanho2">
+                <b className="helvetica fonte_13 cor_3 bold">PERCENTUAL: </b>
+                <b className="helvetica fonte_13 cor_0 lighter"> 00%</b>
+              </div>
+            </div>
+
+            <div className="line">
+              <div className="tamanho2">
+                <b className="helvetica fonte_13 cor_3 bold">NOME: </b>
+                <b className="helvetica fonte_13 cor_0 lighter"> Setor</b>
+              </div>
+              <div className="tamanho2">
+                <b className="helvetica fonte_13 cor_3 bold">RESPONSÁVEL: </b>
+                <b className="helvetica fonte_13 cor_0 lighter"> Nome responsável</b>
+              </div>
+              <div className="tamanho2">
+                <b className="helvetica fonte_13 cor_3 bold">PERCENTUAL: </b>
+                <b className="helvetica fonte_13 cor_0 lighter"> 00%</b>
+              </div>
+            </div>
+
+            <div className="line">
+              <div className="tamanho2">
+                <b className="helvetica fonte_13 cor_3 bold">NOME: </b>
+                <b className="helvetica fonte_13 cor_0 lighter"> Setor</b>
+              </div>
+              <div className="tamanho2">
+                <b className="helvetica fonte_13 cor_3 bold">RESPONSÁVEL: </b>
+                <b className="helvetica fonte_13 cor_0 lighter"> Nome responsável</b>
+              </div>
+              <div className="tamanho2">
+                <b className="helvetica fonte_13 cor_3 bold">PERCENTUAL: </b>
+                <b className="helvetica fonte_13 cor_0 lighter"> 00%</b>
+              </div>
+            </div>
+
+            <div className="line">
+              <div className="tamanho2">
+                <b className="helvetica fonte_13 cor_3 bold">NOME: </b>
+                <b className="helvetica fonte_13 cor_0 lighter"> Setor</b>
+              </div>
+              <div className="tamanho2">
+                <b className="helvetica fonte_13 cor_3 bold">RESPONSÁVEL: </b>
+                <b className="helvetica fonte_13 cor_0 lighter"> Nome responsável</b>
+              </div>
+              <div className="tamanho2">
+                <b className="helvetica fonte_13 cor_3 bold">PERCENTUAL: </b>
+                <b className="helvetica fonte_13 cor_0 lighter"> 00%</b>
+              </div>
+            </div>
+
+
+          </div>
+
+          <div className="content">
+
+            <div className="line">
+              <b className="helvetica fonte_15 cor_3 bold">Centro de custo:</b>
+              <b className="helvetica fonte_15 cor_3 bold">Percentual aprovado:</b>
+            </div>
+
+            <div className="line">
+              <div className="tamanho">
+                <input type="text" placeholder="Digite aqui..." />
+              </div>
+              <div className="tamanho2">
+                <input type="number" placeholder="%"/>
+              </div>
+              <div className="box cor_3f">
+              <RiAddLine color="#fff"/>
+              </div>
+
+            </div>
+
+            <div className="table">
+
+              <div className="header">
+                <div className="title1 bold helvetica cor_0 fonte_15">
+                  Centro de custo
+                </div>
+                <div className="title2 bold helvetica cor_0 fonte_15">
+                    Percentual
+                </div>
+              </div>
+
+              <div className="columns helvetica cor_0 lighter">
+                <div className="column1">
+                  <div className="box cor_3f">
+                  <FiX color="#fff"/>
+                  </div>
+                  Vendas
+                </div>
+
+                <div className="column2">
+                  100%
+                </div>
+              </div>
+
+
+            </div>
+          </div>
+
+          <div className="position">
+            <a href="/cadastrar5">
+            <BotaoAvancar>
+            <p className="helvetica fonte_20 bold">Próximo</p>
+            </BotaoAvancar>
+            </a>
+          </div>
+        </Formulario>
       </>
     );
 }
--- src/pages/Cadastrar6/style.ts
+++ src/pages/Cadastrar6/style.ts
@@ -1,1 +1,179 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+
+export const Titulo = styled.div`
+  margin-top: 30px;
+  margin-left: 30px;
+  display:flex;
+  justify-content: flex-start;
+  flex-wrap: nowrap;
+  flex-direction: row;
+  align-items: center;
+
+  b{
+    margin-left: 5px;
+  }
+`;
+
+export const Formulario = styled.form`
+margin: 20px;
+padding: 60px;
+border:  1px solid #dedede;
+
+display:flex;
+justify-content: space-between;
+align-items: flex-start;
+flex-direction: row;
+flex-wrap: wrap;
+
+p{
+  width: 100%;
+}
+
+.content{
+  height: 320px;
+  display:flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: space-evenly;
+  margin-left: 5px;
+
+  .box{
+    width: 20px;
+    height: 20px;
+    border-radius: 5px;
+    margin: 10px 10px 10px 10px;
+    padding: 2px;
+    box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
+
+
+    &:hover{
+      background: #008EA5;
+      transition: 0.2s;
+    }
+  }
+
+
+.table{
+  filter: drop-shadow(4px 4px 4px rgba(0, 0, 0, 0.25));
+  background: #fff;
+  height: 180px;
+
+  .header{
+    display:flex;
+    width: 100%;
+    height: 50px;
+    align-items: center;
+    background: #EBEBEB;
+    border-bottom-style: solid;
+    border-bottom-width: 0.5px;
+    border-bottom-color: #DCDCDC;
+
+    .title1{
+      width:60%;
+      height: 50px;
+      display:flex;
+      justify-content: center;
+      border-right-style: solid;
+      border-right-width: 0.5px;
+      border-right-color: #DCDCDC;
+      align-items: center;
+    }
+
+    .title2{
+      width:40%;
+      height: 50px;
+      display:flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+  }
+
+  .columns{
+    width:100%;
+    display: flex;
+    background: #fff;
+
+    .column1{
+      width: 60%;
+      height: 30px;
+      display:flex;
+      justify-content: flex-start;
+      align-items: center;
+      border-bottom-style: solid;
+      border-bottom-width: 0.5px;
+      border-bottom-color: #DCDCDC;
+
+
+      }
+
+    .column2{
+      height: 30px;
+      width: 40%;
+      display:flex;
+      justify-content: center;
+      align-items: center;
+
+      border-bottom-style: solid;
+      border-bottom-width: 0.5px;
+      border-bottom-color: #DCDCDC;
+    }
+  }
+
+}
+
+
+  .line{
+    width: 100%;
+    display flex;
+    min-height: 25px;
+
+    flex-direction: row;
+    flex-wrap: nowrap;
+    align-items: center;
+    justify-content: space-between;
+
+    .tamanho{
+      width:45%;
+    }
+
+    .tamanho2{
+      display:flex;
+      justify-content: space-between;
+
+      b{
+        margin: 4px;
+      }
+    }
+
+    input{
+      width: 100%;
+      box-shadow: inset 2px 4px 3px rgba(0,0,0,0.25);
+      border-radius:5px;
+      border: 1px;
+      padding:10px;
+    }
+  }
+}
+
+.position{
+  width:100%;
+    display:flex;
+    justify-content: flex-end;
+
+  }
+
+  input{
+    width: 100%;
+    box-shadow: inset 2px 4px 3px rgba(0,0,0,0.25);
+    border-radius:5px;
+    border: 1px;
+    padding:10px;
+    margin-bottom:15px;
+  }
+
+  b{
+    margin-bottom: 15px;
+    margin-top: 15px;
+  }
+`;
