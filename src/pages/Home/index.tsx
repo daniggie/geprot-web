f4645c1e37f5a87a4823a15fca19fb52de54559b
Brendon <brendon3007gabriel@gmail.com>
2021-08-30 11:42:26 -0300

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import Cards from '../../components/Cards';
-
+import api from '../../services/api';
 import Header from '../../components/Header';
 import Menu from '../../components/Menu';
 
@@ -9,65 +9,65 @@ import FilterSearch from '../../components/FilterSearch';
 import FilterStatus from '../../components/FilterStatus';
 import { FiChevronRight } from 'react-icons/fi';
 
-//interface Projeto {
-//  idprojeto: Number,
-//  nomeprojeto: string,
-//  datainicio: Date,
-  //datafinalizacao: Date,
-  //horasprevistas: Number,
-  //horastrabalhadas: Number,
-  //valorprojeto: Number,
-  //valorutilizado: Number,
-  //valorrestante: Number,
-  //statusprojeto: String
-//}
+interface Projeto {
+  id: Number,
+  nomeprojeto: string,
+  datainicio: Date,
+  datafinalizacao: Date,
+  horasprevistas: Number,
+  horastrabalhadas: Number,
+  valorprojeto: Number,
+  valorutilizado: Number,
+  valorrestante: Number,
+  statusprojeto: String
+}
 
 const Home: React.FC = () => {
-//const [newEndereco , setNewEndereco] = useState('');
-//const [inputError, setInputError] = useState('');
-//const [repositories, setRepositories] = useState<Projeto[]>(() => {
-//  const storageEndereco = localStorage.getItem(
-//    '@EnderecoExplorer:repositories',
-//  );
+const [newEndereco , setNewEndereco] = useState('');
+const [inputError, setInputError] = useState('');
+const [repositories, setRepositories] = useState<Projeto[]>(() => {
+  const storageEndereco = localStorage.getItem(
+    '@EnderecoExplorer:repositories',
+  );
 
-// if(storageEndereco){
-//    return JSON.parse(storageEndereco);
-//  }
-//  return [];
-//});
+ if(storageEndereco){
+    return JSON.parse(storageEndereco);
+  }
+  return [];
+});
 
-//useEffect(() => {
-//  localStorage.setItem(
-//    '@EnderecoExplorer:repositories',
-//    JSON.stringify(repositories)
+useEffect(() => {
+  localStorage.setItem(
+    '@EnderecoExplorer:repositories',
+    JSON.stringify(repositories)
 
-//   );
-//}, [repositories]);
+   );
+}, [repositories]);
 
-//async function handleAddRepository(
-//  event: FormEvent<HTMLFormElement>,
-//): Promise<void> {
-//  event.preventDefault();
+async function handleAddRepository(
+  event: FormEvent<HTMLFormElement>,
+): Promise<void> {
+  event.preventDefault();
 
-//  if(!newEndereco){
-//    setInputError("Campo de busca encontra-se vazio!");
-//    return;
-//  }
+  if(!newEndereco){
+    setInputError("Campo de busca encontra-se vazio!");
+    return;
+  }
 
-// try{
+ try{
 
-//    const response = await api.get<Projeto>(`projetos/listar`);
-//    const repository = response.data;
+    const response = await api.get<Projeto>(`projetos/listar`);
+    const repository = response.data;
 
-//    setRepositories([...repositories, repository]);
-//    setNewEndereco('');
-//    setInputError('');
+    setRepositories([...repositories, repository]);
+    setNewEndereco('');
+    setInputError('');
 
-//  } catch(err){
-//    setInputError("Endereço não encontrado/inexistente");
-//  }
+  } catch(err){
+    setInputError("Endereço não encontrado/inexistente");
+  }
 
-//}
+}
 
   return (
     <>
