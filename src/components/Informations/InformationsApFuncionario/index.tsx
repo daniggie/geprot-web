8692e4539c43aa045f1cc433973d7a5b50680b7e
Brendon <brendon3007gabriel@gmail.com>
2021-09-13 14:47:29 -0300

    Update index.tsx

--- src/components/Informations/InformationsApFuncionario/index.tsx
+++ src/components/Informations/InformationsApFuncionario/index.tsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Content } from "./style";
 import { AiFillCheckCircle, AiFillExclamationCircle, AiFillCloseCircle } from "react-icons/ai";
 import { CgSearchLoading } from "react-icons/cg";
 import IconeAprovamento from "../../IconeAprovamento";
+import api from "../../../services/api";
+import { useParams } from "react-router";
+
+interface Card {
+  todosApontamentos: [
+    {
+      "data": "2021-02-09",
+      "descricao_trabalho": "Trabalho legalzin",
+      "quantidade_horas": 30,
+      "status": "REPROVADO"
+    }
+  ],
+}
 
 const InformationsApFuncionario: React.FC = () => {
+  const { id }: {id:string} = useParams();
+  const { consultorId }: {consultorId:string} = useParams();
+  const [valores, setValores ] = useState<Card>();
+
+  useEffect(() => {
+    async function carregaDados(): Promise<void>  {
+      await api.get(`horas/listar/${id}/${consultorId}`).then(response => {
+        setValores(response.data)
+      })
+    }
+    carregaDados();
+  }, [ ])
+  console.log(valores)
+
+  const [secao] = useState(() => {
+    let usuario = localStorage.getItem('@Logistica:usuario');
+
+    if(usuario) {
+        let languageObject = JSON.parse(usuario);
+        return languageObject;
+    }
+});
+
     return (
         <>
         <Content>
