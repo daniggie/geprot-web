849c0331c40ece35c1ff0f044951e0fe20fabba3
daniggie <dani.gross53@gmail.com>
2021-09-16 11:05:14 -0300

    inicio overlay atribuir

--- src/components/Atribuicao/index.tsx
+++ src/components/Atribuicao/index.tsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "./style";
+import arrowleft from "../../icons/arrowleft.svg";
+import BotaoEnviar from "../Buttons/ButtonEnviar";
 
 const Atribuicao: React.FC = () => {
+  const [ overlay, setOverlay ] = useState(true);
+  const addOverlay = () => {
+    if(!overlay){
+     setOverlay(true);
+    }else{
+      setOverlay(false);
+    }
+
+    return overlay;
+
+  };
   return (
     <>
-    <Container>
+    <Container overlay={overlay}>
       <button className="cor_000">Atribuir</button>
+      <div id="atribuir">
+        <div className="overlay">
+            <img src={arrowleft} alt="" onClick={addOverlay}/>
+            <p className="helvetica cor_5 fonte_20">Horas:</p>
+            <input placeholder="" type="time"/>
+            <BotaoEnviar/>
+        </div>
+
+      </div>
     </Container>
+
+
     </>
   )
 };
--- src/components/Atribuicao/style.ts
+++ src/components/Atribuicao/style.ts
@@ -1,7 +1,11 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { shade } from "polished";
 
-export const Container=styled.div`
+interface TableProps{
+  overlay: boolean;
+};
+
+export const Container=styled.div<TableProps>`
   width:10%;
   height: 50px;
   display:flex;
@@ -21,4 +25,63 @@ export const Container=styled.div`
       background: ${shade(0.2, "#30a130")};
     }
   }
+
+  #atribuir{
+
+    ${(props) =>
+      props.overlay &&
+      css`
+        visibility: hidden;
+        opacity: 0;
+      `}
+
+    ${(props) =>
+        !props.overlay &&
+        css`
+          visibility: visible;
+          opacity: 1;
+        `}
+
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.30);
+  transition: 0.5s;
+
+  .overlay{
+    position: absolute;
+    width: 40%;
+    height: 300px;
+    border-radius: 10px;
+    background: #fff;
+    padding: 15px;
+    display:flex;
+    flex-wrap: wrap;
+    justify-content: flex-end;
+
+    p{
+      display:flex;
+      justify-content: center;
+      align-items: center;
+      width: 95%;
+      height: 30px;
+    }
+
+    img{
+      width: 3%;
+      height: 30px;
+      cursor: pointer;
+    }
+
+    input{
+
+    }
+}
+}
 `;
