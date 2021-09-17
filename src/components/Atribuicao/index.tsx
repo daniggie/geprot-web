6b6a090ab4cef76275aaacd6c35b1a371e01dce6
Brayan <brayan.11fera@gmail.com>
2021-09-17 10:57:03 -0300

    n ta cadastrado

--- src/components/Atribuicao/index.tsx
+++ src/components/Atribuicao/index.tsx
@@ -3,7 +3,6 @@ import { Container } from "./style";
 import arrowleft from "../../icons/arrowleft.svg";
 import { RiCheckLine } from "react-icons/ri";
 import api from "../../services/api";
-import { useCallback } from "hoist-non-react-statics/node_modules/@types/react";
 
 interface AtribuicaoProps {
   projetos_id: number,
@@ -16,8 +15,27 @@ interface AlocarProps {
   limiteHoras: number
 }
 
+
+
 // valores chegando pelo botao, falta fazer a funcao para alocar
 const Atribuicao: React.FC<AtribuicaoProps> = ({projetos_id, usuarios_id}) => {
+  function Alocar(usuarios_id: number, projetos_id: number) {
+    console.log("bomdia")
+    const alocarHoras = {
+      usuarios_id: 0,
+      projetos_id: 0,
+      limiteHoras: 0,
+    }
+
+    alocarHoras.projetos_id = projetos_id;
+    alocarHoras.usuarios_id = usuarios_id;
+    alocarHoras.limiteHoras = parseInt((document.getElementById('inputLegal') as HTMLInputElement).value);
+
+    console.log(alocarHoras)
+    api.post(`consultores/alocar`, JSON.stringify(alocarHoras))
+  }
+
+
   const [ overlay, setOverlay ] = useState(true);
   const addOverlay = () => {
     if(!overlay){
@@ -40,8 +58,8 @@ const Atribuicao: React.FC<AtribuicaoProps> = ({projetos_id, usuarios_id}) => {
         <div className="overlay">
             <img src={arrowleft} alt="" onClick={addOverlay}/>
             <p className="helvetica cor_5 fonte_20">Horas:</p>
-            <input placeholder="0H" type="number"/>
-            <button><RiCheckLine size={ 25 } color="#fff"/></button>
+            <input id="inputLegal" placeholder="0H" type="number"/>
+            <button onClick={() => Alocar(parseInt(usuarios_id), projetos_id)}><RiCheckLine size={ 25 } color="#fff"/></button>
 
 
         </div>
