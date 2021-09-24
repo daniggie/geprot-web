7423521a04af9e59d4cda70d868c2b9ff581d69d
daniggie <dani.gross53@gmail.com>
2021-09-24 09:24:27 -0300

    botão aprovado

--- src/components/Buttons/ButtonAprove/style.ts
+++ src/components/Buttons/ButtonAprove/style.ts
@@ -20,3 +20,22 @@ p{
   justify-content: center;
 }
 `;
+
+export const ContainerAprovado = styled.div`
+width: 205px;
+background: #c4c4c4;
+border-radius: 5px;
+border:0px;
+display:flex;
+justify-content: center;
+align-items: center;
+padding: 10px;
+cursor: not-allowed;
+
+p{
+  font-size:20px;
+  color:#fff;
+  display:flex;
+  justify-content: center;
+}
+`;
