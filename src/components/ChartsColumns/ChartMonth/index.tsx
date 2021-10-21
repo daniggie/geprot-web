eaebed5002b005b495bc3ea8c00147aac42d663c
gui352 <lintzguigui1323@gmail.com>
2021-10-21 16:50:32 -0300

    Update index.tsx

--- src/components/ChartsColumns/ChartMonth/index.tsx
+++ src/components/ChartsColumns/ChartMonth/index.tsx
@@ -1,70 +1,67 @@
 import React, {useState, useEffect} from "react";
-import { useParams } from "react-router";
 import api from "../../../services/api";
 import { Chart } from "react-google-charts";
 
-
-
-  interface DashboardConcluidos {
-    mesAno: string;
-    quantidade: number;
-  }
+interface DashboardConcluidos {
+  mesAno: string;
+  quantidade: number;
+}
 
 const ChartMonth: React.FC = () => {
-    const [valoresConcluidos, setValoresConcluidos] = useState<DashboardConcluidos[]>([])
-    const token = localStorage.getItem("@Geprot:token");
-    let config = {
-      headers: { Authorization: `Bearer ${token}` },
-    };
+  const [valoresConcluidos, setValoresConcluidos] = useState<DashboardConcluidos[]>([])
+  const token = localStorage.getItem("@Geprot:token");
+  let config = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
 
-    const [secao] = useState(() => {
-      let usuario = localStorage.getItem('@Geprot:gestor');
-
-      if(usuario) {
-          let languageObject = JSON.parse(usuario);
-          return languageObject;
-      }
-    });
-
-    async function buscarValores1() {
-      var response = await api.get(`dashboard/concluidos/ultimoMes/${secao.secao.id ? secao.secao.id : null}`, config).then(response => {
-        setValoresConcluidos(response.data)
-      })
+  const [secao] = useState(() => {
+    let usuario = localStorage.getItem('@Geprot:gestor');
+    if(usuario) {
+      let languageObject = JSON.parse(usuario);
+      return languageObject;
     }
+  });
 
-    useEffect(() => {
-      buscarValores1()
-    }, [])
+  async function buscarValores1() {
+    var response = await api.get(`dashboard/concluidos/ultimoMes/${secao.secao.id ? secao.secao.id : null}`, config).then(response => {
+      setValoresConcluidos(response.data)
+    })
+  }
 
-    return(
-        <Chart
-            width={"100%"}
-            height={"180px"}
-            chartType="ColumnChart"
-            loader={<p>Loading Chart</p>}
+  useEffect(() => {
+    buscarValores1()
+  }, [])
 
-            data={[
-              ['Weeks', 'Projetos concluidos', { role: 'style' }],
-              [valoresConcluidos[3] ? valoresConcluidos[3].mesAno : undefined, valoresConcluidos[3] ? valoresConcluidos[3].quantidade : undefined, "#0091BD"],
-              [valoresConcluidos[2] ? valoresConcluidos[2].mesAno : undefined, valoresConcluidos[2] ? valoresConcluidos[2].quantidade : undefined,'#2382BA'],
-              [valoresConcluidos[1] ? valoresConcluidos[1].mesAno : undefined, valoresConcluidos[1] ? valoresConcluidos[1].quantidade : undefined,"#0091BD"],
-              [valoresConcluidos[0] ? valoresConcluidos[0].mesAno : undefined, valoresConcluidos[0] ? valoresConcluidos[0].quantidade : undefined,'#2382BA'],
-            ]}
+  return(
+    <Chart
+      width={"100%"}
+      height={"180px"}
+      chartType="ColumnChart"
+      loader={<p>Loading Chart</p>}
 
-            options={{
-                legend: 'none',
-                columnSliceTextStyle: {
-                    color: '#828282',
-                },
-                chartArea:{
-                    left:40,
-                    top:10,
-                    width:'92%',
-                    height:'80%'
-                }
-            }}
-        />
-    );
+      data={[
+        ['Weeks', 'Projetos concluidos', { role: 'style' }],
+        [valoresConcluidos[3] ? valoresConcluidos[3].mesAno : undefined, valoresConcluidos[3] ? valoresConcluidos[3].quantidade : undefined, "#0091BD"],
+        [valoresConcluidos[2] ? valoresConcluidos[2].mesAno : undefined, valoresConcluidos[2] ? valoresConcluidos[2].quantidade : undefined,'#2382BA'],
+        [valoresConcluidos[1] ? valoresConcluidos[1].mesAno : undefined, valoresConcluidos[1] ? valoresConcluidos[1].quantidade : undefined,"#0091BD"],
+        [valoresConcluidos[0] ? valoresConcluidos[0].mesAno : undefined, valoresConcluidos[0] ? valoresConcluidos[0].quantidade : undefined,'#2382BA'],
+      ]}
+
+      options={{
+        legend: 'none',
+        columnSliceTextStyle: {
+        color: '#828282',
+      },
+
+      chartArea:{
+        left:40,
+        top:10,
+        width:'92%',
+        height:'80%'
+      }
+      }}
+    />
+  );
 };
 
 export default ChartMonth;
