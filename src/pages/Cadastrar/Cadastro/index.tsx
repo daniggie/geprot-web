5dcf5e0da8faab6775c6ffa9abf04b8e292bdbfd
gui352 <lintzguigui1323@gmail.com>
2021-10-25 11:40:07 -0300

    tela de cadastro de projeto atualizada, precisa terminar 25102021

--- src/pages/Cadastrar/Cadastro/index.tsx
+++ src/pages/Cadastrar/Cadastro/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, FormEvent } from 'react';
-import { All } from './styles';
+import { All, Container } from './styles';
 import { Form } from "@unform/web";
 import Header from '../../../components/Header';
 import ButtonRegister from '../../../components/Buttons/ButtonRegister';
@@ -7,6 +7,7 @@ import Menu from '../../../components/Menu';
 import ButtonCancel from '../../../components/Buttons/ButtonCancel';
 import { RiAddLine } from 'react-icons/ri';
 import { FiX } from 'react-icons/fi';
+import { BiReceipt } from 'react-icons/bi';
 
 const Cadastro:React.FC = () => {
   return( 
@@ -17,56 +18,63 @@ const Cadastro:React.FC = () => {
         <div className="title">
           <b className="helvetica fonte_20 cor_5">CADASTRO DE PROJETO</b>
         </div>
-        <form>
-          <div className="column">
-            <div className="line">
-              <p className="helvetica fonte_15 cor_5 bold">Nome do projeto:</p>
-              <input name="nomeProjeto" id="nomeProjeto" placeholder="Digite o nome..."/>
-            </div>
-
-            <div className="line">
-              <p className="helvetica fonte_15 cor_5 bold">Nome do solicitante:</p>
-              <input name="nomeSolicitante" id="nomeSolicitante" placeholder="Digite o nome..."/>
-            </div>
-
-            <div className="line">
-              <p className="helvetica fonte_15 cor_5 bold">Nome do responsável:</p>
-              <input name="nomeResponsavel" id="nomeResponsavel" placeholder="Digite o nome..."/>
-            </div>
-
-            <div className="line">
-              <div>
-                <p className="helvetica fonte_15 cor_5 bold">Data de inicio:</p>
-                <input type="date"  id="dataInicio"/>
+        <Container>
+          <form>
+            <div className="column">
+              <div className="line">
+                  <div className="download">
+                    <p className="helvetica fonte_13 cor_0 bold">ATA_COMISSÃO_001</p>
+                    <BiReceipt color="#00579D" size={25}/>
+                  </div>
               </div>
-              <div>
-                <p className="helvetica fonte_15 cor_5 bold">Data de encerramento:</p>
-                <input type="date" id="dataFinalizacao" />
-              </div>
-            </div>
 
-            <div className="line">
-              <p className="helvetica fonte_15 cor_5 bold">Descrição</p>
-              <textarea id="descricao" className="helvetica"></textarea>
-            </div>
-          </div>
-
-          <div className="column">
-            <div className="line">
-              <div>
-                <p className="helvetica fonte_15 cor_5 bold">Consultor:</p>
-                <input id="idConsultor" type="number" placeholder="Digite o ID..."/>
+              <div className="line">
+                <p className="helvetica fonte_15 cor_5 bold">Nome do projeto:</p>
+                <input name="nomeProjeto" id="nomeProjeto" placeholder="Digite o nome..."/>
               </div>
-              <div>
-                <p className="helvetica fonte_15 cor_5 bold">Horas:</p>
-                <input id="horasConsultor" type="number" placeholder="1" />
-                <div className="box cor_6f" >
-                  <RiAddLine color="#fff"/>
+
+              <div className="line">
+                <p className="helvetica fonte_15 cor_5 bold">Nome do solicitante:</p>
+                <input name="nomeSolicitante" id="nomeSolicitante" placeholder="Digite o nome..."/>
+              </div>
+
+              <div className="line">
+                <p className="helvetica fonte_15 cor_5 bold">Nome do responsável:</p>
+                <input name="nomeResponsavel" id="nomeResponsavel" placeholder="Digite o nome..."/>
+              </div>
+
+              <div className="line">
+                <div className="float">
+                  <p className="helvetica fonte_15 cor_5 bold">Data de inicio:</p>
+                  <input type="date"  id="dataInicio"/>
+                </div>
+                <div className="float">
+                  <p className="helvetica fonte_15 cor_5 bold">Data de encerramento:</p>
+                  <input type="date" id="dataFinalizacao" />
                 </div>
               </div>
+
+              <div className="line">
+                <p className="helvetica fonte_15 cor_5 bold">Descrição</p>
+                <textarea id="descricao" className="helvetica"></textarea>
+              </div>
             </div>
 
-            <div className="line">
+            <div className="column">
+              <div className="line">
+                <div className="float">
+                  <p className="helvetica fonte_15 cor_5 bold">Consultor:</p>
+                  <input id="idConsultor" type="number" placeholder="Digite o ID..."/>
+                </div>
+                <div className="float">
+                  <p className="helvetica fonte_15 cor_5 bold">Horas:</p>
+                  <input id="horasConsultor" type="number" placeholder="1" />
+                  <div className="boxAdd cor_6f" >
+                    <RiAddLine color="#fff"/>
+                  </div>
+                </div>
+              </div>
+
               <div className="table">
                 <div className="header">
                   <div className="title3 bold helvetica cor_0 fonte_15">
@@ -82,9 +90,9 @@ const Cadastro:React.FC = () => {
 
                 <div className="columns helvetica cor_0 lighter" >
                   <div className="column3">
-                    <div className="box cor_6f">
+                    <div className="boxEx cor_6f">
                       <FiX color="#fff"/>
-                    </div>     
+                    </div>
                   </div>
                   <div className="column1">  
                   </div>
@@ -92,28 +100,44 @@ const Cadastro:React.FC = () => {
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div className="line">
-              <div>
-                <p className="helvetica fonte_15 cor_5 bold">Centro de Custo:</p>
-                <input id="idConsultor" type="number" placeholder="Digite o ID..."/>
-              </div>
-              <div>
-                <p className="helvetica fonte_15 cor_5 bold">Percentual Aprovado:</p>
-                <input id="horasConsultor" type="number" placeholder="%" />
-                <div className="box cor_6f" >
-                  <RiAddLine color="#fff"/>
+              <div className="line">
+                <div className="float">
+                  <p className="helvetica fonte_15 cor_5 bold">Centro de Custo:</p>
+                  <input id="idConsultor" type="number" placeholder="Digite o ID..."/>
+                </div>
+                <div className="float">
+                  <p className="helvetica fonte_15 cor_5 bold">Percentual Aprovado:</p>
+                  <input id="horasConsultor" type="number" placeholder="%" />
+                  <div className="boxAdd cor_6f" >
+                    <RiAddLine color="#fff"/>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
 
-          <div className="position">
-            <ButtonCancel/>
-            <ButtonRegister type="submit">Cadastrar</ButtonRegister>
-          </div>
-        </form>
+              <div className="table">
+
+              <div className="header">
+                <div className="title1 bold helvetica cor_0 fonte_15">
+                  Centro de custo
+                </div>
+                <div className="title1 bold helvetica cor_0 fonte_15">
+                    Percentual
+                </div>
+              </div>
+
+              <div className="columns helvetica cor_0 lighter">
+                <div className="column1">
+                  <div className="boxEx cor_6f">
+                    <FiX color="#fff"/>
+                  </div>
+                </div>
+                <div className="column2"></div>
+              </div>
+            </div>
+            </div>
+          </form>
+        </Container>
       <Menu/>
     </All>
   </>
--- src/pages/Cadastrar/Cadastro/styles.ts
+++ src/pages/Cadastrar/Cadastro/styles.ts
@@ -17,36 +17,195 @@ export const All= styled.div`
     margin-bottom: 15px;
     margin-left: 120px;
   }
+`;
+
+export const Container=styled.div`
+  width: 74%;
+  max-height: 400px;
+  padding: 25px;
+  margin: 0 85px 0 120px;
+
+  border: 0.5px;
+  border-style: solid;
+  border-color: #c4c4c4;
+  border-radius: 4px;
+
+  overflow: auto;
+
+  .boxAdd{
+    width: 20px;
+    height: 20px;
+    border-radius: 5px;
+    margin: -19px 0px 0px 210px;
+    padding: 2px;
+    cursor: pointer;
+
+    &:hover{
+      background: #008EA5;
+      transition: 0.2s;
+      cursor: pointer;
+    }
+  }
+
+  .boxEx{
+    width: 20px;
+    height: 20px;
+    border-radius: 5px;
+    padding: 2px;
+    cursor: pointer;
+    margin: 0px 0px 0px 0px;
+
+    &:hover{
+      background: #008EA5;
+      transition: 0.2s;
+      cursor: pointer;
+    }
+  }
 
   form{
-    width: 70%;
-    max-height: 400px;
-    padding: 15px;
-    margin: 0 153px 0 120px;
-
     display: flex;
     flex-wrap: wrap;
     align-content: space-around;
     justify-content: center;
 
-    border: 0.5px;
-    border-style: solid;
-    border-color: #c4c4c4;
-    border-radius: 4px;
-
-    overflow: auto;
-
     .column{
       width: 48%;
-      height: 500px;
+      height: 550px;
       align-items: center;
       align-content: space-around;
       justify-content: space-around;
 
+      .table{
+        background: #fff;
+        height: 200px;
+        border: 1px solid #dedede;
+        border-radius: 3px;
+        margin-bottom: 20px;
+        overflow: auto;
+      
+        .header{
+          display:flex;
+          width: 100%;
+          height: 50px;
+          align-items: center;
+          background: #EBEBEB;
+          border-bottom-style: solid;
+          border-bottom-width: 0.5px;
+          border-bottom-color: #DCDCDC;
+      
+          .title1{
+            width:50%;
+            height: 50px;
+            display:flex;
+            justify-content: center;
+            border-right-style: solid;
+            border-right-width: 0.5px;
+            border-right-color: #DCDCDC;
+            align-items: center;
+          }
+      
+          .title2{
+            width:28%;
+            height: 50px;
+            display:flex;
+            justify-content: center;
+            align-items: center;
+          }
+      
+          .title3{
+            width:22%;
+            height: 50px;
+            display:flex;
+            justify-content: center;
+            align-items: center;
+            border-right-style: solid;
+            border-right-width: 0.5px;
+            border-right-color: #DCDCDC;
+          }
+      
+      
+        }
+      
+        .columns{
+          width:100%;
+          display: flex;
+          background: #fff;
+      
+          .column1{
+            width: 50%;
+            height: 30px;
+            display:flex;
+            justify-content: center;
+            align-items: center;
+            border-bottom-style: solid;
+            border-bottom-width: 0.5px;
+            border-bottom-color: #DCDCDC;
+          }
+      
+          .column2{
+            height: 30px;
+            width: 30%;
+            display:flex;
+            justify-content: center;
+            align-items: center;
+      
+            border-bottom-style: solid;
+            border-bottom-width: 0.5px;
+            border-bottom-color: #DCDCDC;
+          }
+      
+          .column3{
+            height: 30px;
+            width: 20%;
+            display:flex;
+            justify-content: center;
+            align-items: center;
+      
+            border-bottom-style: solid;
+            border-bottom-width: 0.5px;
+            border-bottom-color: #DCDCDC;
+          }
+        }
+      }
+
       .line{
         width: 100%;
-        height: 80px;
-        margin-bottom: 15px;
+        height: 40px;
+        margin-bottom: 20px;
+
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: row;
+        align-items: flex-start;
+        align-content: flex-start;
+        justify-content: flex-start;
+
+        .download{
+          width: 70%;
+          background: #FFFFFF;
+          border: 1px solid rgba(0, 0, 0, 0.2);
+          box-sizing: border-box;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          border-radius: 4px;
+          padding:5px 10px;
+          cursor: pointer;
+
+          p{
+            margin: 11px 0px 8px 0px;
+          }
+    
+          &:hover{
+            opacity: 0.7;
+            transition: 0.2s;
+    
+          }
+        }
+
+        .float{
+          width: 50%;
+        }
 
         textarea{
           width: 90%;
@@ -59,59 +218,11 @@ export const All= styled.div`
           resize: none;
         }
 
-        .box{
-          width: 20px;
-          height: 20px;
-          border-radius: 5px;
-          margin: 10px 10px 10px 10px;
-          padding: 2px;
-          cursor: pointer;
-    
-          &:hover{
-            background: #008EA5;
-            transition: 0.2s;
-            cursor: pointer;
-          }
-        }
-
         p{
           display: flex;
           width: 80%;
           justify-content: start;
           margin-bottom: 5px;
-        }
-      }
-    }
-
-    .position{
-      width:100%;
-      display:flex;
-      justify-content: space-between;
-      padding: 0px 20px 0px 20px;
-
-      button{
-        width: 205px;
-        background: #00579D;
-        border-radius: 5px;
-        border:0px;
-        float:right;
-        display:flex;
-        justify-content: center;
-        align-items: center;
-        padding: 10px;
-        color: #fff;
-
-        &:hover{
-          background: #0078bd;
-          transition: 0.4s;
-        }
-
-        p{
-          font-size:20px;
-          color:#fff;
-          display:flex;
-          justify-content: center;
-          font-style:normal;
         }
       }
     }
