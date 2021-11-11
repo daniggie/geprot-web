7ab56a50202110914a9459deff0ff659a75f748f
daniggie <dani.gross53@gmail.com>
2021-11-11 10:00:19 -0300

    Add de codigo translate

--- src/translate/languages/index.js
+++ src/translate/languages/index.js
@@ -1,7 +1,11 @@
 import {messages as portugueseMessages} from "./pt";
+import {messages as englishMessages} from "./en";
+import {messages as espanishMessages} from "./es";
 
 const messages = {
     ...portugueseMessages,
+    ...englishMessages,
+    ...espanishMessages
 }
 
 export {messages}
