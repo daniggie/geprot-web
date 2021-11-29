a472956d4610ba40d48b56e0497297067eb73cae
daniggie <dani.gross53@gmail.com>
2021-11-29 10:16:47 -0300

    Skill componentizada

---
+++ src/components/Atribuicao/Skills/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container=styled.div`
+    margin: 0;
+    padding: 0;
+
+    width: calc(100%/3);
+    height: 10%;
+    
+    input, label{
+        margin-left: 15px;
+    }
+`;
