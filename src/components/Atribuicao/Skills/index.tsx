4a5a4de4d95110c5bdb55c3fcd6733719589d040
daniggie <dani.gross53@gmail.com>
2021-11-29 10:16:27 -0300

    Skill como componente

---
+++ src/components/Atribuicao/Skills/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "./style";
+
+const Skills: React.FC = () => {
+    return(
+      <Container>
+            <input type="checkbox" id="vehicle1" name="vehicle1" value=""/>
+            {/*Chamar NAMETEST em API */}
+            <label>Nametest</label>
+      </Container>
+    );
+  };
+  
+export default Skills;
